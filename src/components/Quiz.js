a93c594fba4b137a19f72c45a0addd9929fdf522
zheta88 <zytamayo@misena.edu.co>
2019-11-07 22:15:22 -0500

    se crea component Quiz

---
+++ src/components/Quiz.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { ReactComponent } from '*.svg';
+
+class  Quiz extends React.Component{
+    state ={
+        userAnswer: null,
+        currentQuestion: 0,
+        options: []
+    }
+
+render (){
+
+    return(
+        <div className="App">
+            
+            content
+
+        </div>
+    )
+}
+
+}
+export default Quiz;
